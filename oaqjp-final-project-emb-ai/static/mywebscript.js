8f08c5f0f3e76d04cbe778eb797f15199718149d
jmill29 <jacoblmiller.jlm@gmail.com>
2024-04-30 17:12:04 -0400

    Added user interface

--- oaqjp-final-project-emb-ai/static/mywebscript.js
+++ oaqjp-final-project-emb-ai/static/mywebscript.js
@@ -4,7 +4,23 @@ let RunSentimentAnalysis = ()=>{
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("system_response").innerHTML = xhttp.responseText;
+            let res = JSON.parse(xhttp.responseText);
+            let responseText = 'For the given statement, the system response is ';
+            for (let i = 0; i < Object.keys(res).length; i++) {
+                if (Object.keys(res)[i] !== 'dominant_emotion') {
+                    if (i == Object.keys(res).length-2) {
+                        responseText += `'${Object.keys(res)[i]}': ${res[Object.keys(res)[i]]} and `;
+                    } else if (i == Object.keys(res).length-1) {
+                        responseText += `'${Object.keys(res)[i]}': ${res[Object.keys(res)[i]]}. `;
+                    } else {
+                        responseText += `'${Object.keys(res)[i]}': ${res[Object.keys(res)[i]]}, `;
+                    }
+                }
+            }
+            responseText += `The dominant emotion is  <strong>${res['dominant_emotion']}</strong>.`
+            document.getElementById("system_response").innerHTML = responseText;
+        } else {
+            document.getElementById("system_response").innerHTML = JSON.parse(xhttp.responseText)['message'];
         }
     };
     xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
